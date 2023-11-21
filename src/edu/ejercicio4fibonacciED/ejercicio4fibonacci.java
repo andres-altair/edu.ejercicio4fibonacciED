114467ee446d8effba1d64b62a2ab71434d416a4
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-11-21 12:22:59 +0100

    no message

---
+++ src/edu/ejercicio4fibonacciED/ejercicio4fibonacci.java
@@ -0,0 +1,13 @@
+package edu.ejercicio4fibonacciED;
+
+import java.util.Scanner;
+
+public class ejercicio4fibonacci {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("¿Cuántas posiciones de la serie de Fibonacci quiere calcular?");
+		int i = scan.nextInt();
+	}
+
+}
